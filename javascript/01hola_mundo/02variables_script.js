e923f3c578f05179ec748e8b76003861f56a70de
David Hernández <daernandez@gmail.com>
2019-01-31 09:21:07 +0100

    variables files added

---
+++ javascript/01hola_mundo/02variables_script.js
@@ -0,0 +1,12 @@
+// Javascript
+
+/* este es un comentario de javascript
+    de varias líneas                    */
+var texto = "Hola Mundo, soy una variable";
+document.write(texto);
+document.write('<br>');
+texto = "JOJOJO... ¡CENSURADO!"
+document.write(texto);
+window.alert(texto);
+console.log(texto);
+
