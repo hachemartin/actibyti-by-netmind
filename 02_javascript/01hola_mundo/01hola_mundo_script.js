26606d9e246acb86440e176ad9f530b375046a35
David Hernández <daernandez@gmail.com>
2019-03-13 08:54:33 +0100

    file added

---
+++ 02_javascript/01hola_mundo/01hola_mundo_script.js
@@ -0,0 +1,9 @@
+// Javascript
+
+/* este es un comentario de javascript
+    de varias líneas                    */
+
+document.write('Hola Mundo en el documento');
+window.alert('Alerta Hola Mundo');
+console.log('Hola Mundo en consola');
+
